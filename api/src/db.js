c85175d4a57d109825a2191eae1cd2877ef80836
alejandrovc1 <alejandrovc-@outlook.com>
2022-08-17 00:19:16 -0500

    Backend v1.2

--- api/src/db.js
+++ api/src/db.js
@@ -6,18 +6,18 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-// const sequelize = new Sequelize(`${DB_NAME}`,`${DB_USER}`,`${DB_PASSWORD}`,{
-//   host: DB_HOST,
-//   dialect: 'postgres',
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
-const sequelize = new Sequelize('dogs','postgres','alejandrogamer',{
+const sequelize = new Sequelize(`${DB_NAME}`,`${DB_USER}`,`${DB_PASSWORD}`,{
   host: DB_HOST,
   dialect: 'postgres',
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+// const sequelize = new Sequelize('dogs','postgres','alejandrogamer',{
+//   host: DB_HOST,
+//   dialect: 'postgres',
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
