4eccef7ab416b21a09c0877caade17d95b9e6c6e
alejandrovc1 <alejandrovc-@outlook.com>
2022-08-18 01:45:43 -0500

    Backend v1.3

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,67 +8,66 @@ const router = Router();
 
 function getBreeds() {
     let breedsAPI = axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
-    .then( async function (response) {
-        var breeds = response.data.map(breed => (
-            {   
-                id: breed.id,
-                name: breed.name.toLowerCase(),
-                weight: breed.weight.metric.replace('NaN - ','').replace('NaN','26'),
-                height: breed.height.metric,
-                life_span: breed.life_span,
-                image: breed.image.url,
-                temperament: breed.temperament,
-                origin: "api"
-            }))
-        const breedsFromDb = (await Dog.findAll({ include: "temperaments" })).map(breed => (
-            {
-                id: breed.id,
-                name: breed.name,
-                weight: breed.weight,
-                height: breed.height,
-                life_span: breed.life_span,
-                image: breed.image,
-                temperament: breed.temperaments ?
-                    (([tempss] = breed.temperaments).map(e => e.name)).join(', ')  
-                    : "",
-                origin: breed.origin
-            }))
-        return allBreeds = breeds.concat(breedsFromDb).sort((a, b) => a.name.localeCompare(b.name))
-    })
+        .then(async function (response) {
+            var breeds = response.data.map(breed => (
+                {
+                    id: breed.id,
+                    name: breed.name.toLowerCase(),
+                    weight: breed.weight.metric.replace('NaN - ', '').replace('NaN', '26'),
+                    height: breed.height.metric,
+                    life_span: breed.life_span,
+                    image: breed.image.url,
+                    temperament: breed.temperament,
+                    origin: "api"
+                }))
+            const breedsFromDb = (await Dog.findAll({ include: "temperaments" })).map(breed => (
+                {
+                    id: breed.id,
+                    name: breed.name,
+                    weight: breed.weight,
+                    height: breed.height,
+                    life_span: breed.life_span,
+                    image: breed.image,
+                    temperament: breed.temperaments ?
+                        (([tempss] = breed.temperaments).map(e => e.name)).join(', ')
+                        : "",
+                    origin: breed.origin
+                }))
+            return allBreeds = breeds.concat(breedsFromDb).sort((a, b) => a.name.localeCompare(b.name))
+        })
     return breedsAPI
 }
 
 router.get('/dogs', function (req, res) {
 
     getBreeds()
-    .then(() =>
-    {
-        if (req.query.hasOwnProperty('name')) {
-            let { name } = req.query
-        
-            let found = false
-            let breedsWithName= []
-            for (let i = 0; i < allBreeds.length; i++) {
-                if (allBreeds[i].name.includes(name)) {
-                    breedsWithName.push(allBreeds[i])
-                    found = true
+        .then(() => {
+            if (req.query.hasOwnProperty('name')) {
+                let { name } = req.query
+
+                let found = false
+                let breedsWithName = []
+                for (let i = 0; i < allBreeds.length; i++) {
+                    if (allBreeds[i].name.includes(name)) {
+                        breedsWithName.push(allBreeds[i])
+                        found = true
+                    }
+                }
+                if (found) {
+                    res.status(200).send(breedsWithName)
+                    
+                }
+                else {
+                    res.status(404).send('<h1>ERROR: breed not found. Try again!</h1>')
                 }
             }
-            if (found) {
-                res.status(200).send(breedsWithName)
-                
-            }
             else {
-                res.status(404).send('<h1>ERROR: breed not found. Try again!</h1>')
+                res.json(allBreeds)
             }
-        }
-        else {
-            res.json(allBreeds)
-        }
-    })
-    .catch((e) => {
-        console.log(e)
-    })
+        })
+        .catch((e) => {
+            console.log(e)
+        })
 })
 
 router.get('/dogs/:idRaza', function (req, res) {
@@ -76,9 +75,9 @@ router.get('/dogs/:idRaza', function (req, res) {
     getBreeds()
         .then(() => {
             for (let j = 0; j < allBreeds.length; j++) {
-            if (allBreeds[j].id == idRaza) { return res.json(allBreeds[j]) }
-            } 
-            return res.status(404).send('<h1>ERROR: breed not found. Try again!</h1>') 
+                if (allBreeds[j].id == idRaza) { return res.json(allBreeds[j]) }
+            }
+            return res.status(404).send('<h1>ERROR: breed not found. Try again!</h1>')
         })
 })
 
@@ -86,10 +85,10 @@ router.get('/temperament', async function (req, res) {
     let temps = await Temperament.findAll()
 
     if (temps.length > 0) {
-      res.json(temps)  
+        res.json(temps)
     } else {
         res.status(404).send("Error getting temperaments")
-    } 
+    }
 })
 
 router.post('/dog', async function (req, res) {
@@ -99,11 +98,11 @@ router.post('/dog', async function (req, res) {
     for (let i = 0; i < temps.length; i++) {
         let tempSearched = await Temperament.findOne({ where: { name: temps[i] } });
         if (tempSearched !== null) {
-            arrTemps.push (tempSearched.dataValues.id)
-        }        
+            arrTemps.push(tempSearched.dataValues.id)
+        }
     }
     try {
-        
+
         const dog = await Dog.create({
             name,
             weight,
@@ -113,7 +112,7 @@ router.post('/dog', async function (req, res) {
             origin: "db"
         });
         arrTemps.forEach(async (temp) => (await dog.addTemperaments(temp)))
-     
+
         res.json(dog)
     }
     catch (e) {
@@ -121,5 +120,27 @@ router.post('/dog', async function (req, res) {
         res.status(404).send('An error has ocurred', e)
     }
 })
- 
+
+//Promise
+// function getData() {
+//     return new Promise((resolve, reject) => {
+//         if (data.length === 0) {
+//             reject(new Error('Data is empty'))
+//         }
+//         setTimeout(() => {
+//             resolve(data);
+//         }, 2000)
+//     })
+// }
+
+// getData()
+//     .then((response) => console.log(response))
+//     .catch((err) => console.log(err.message))
+
+//Async await 
+//async function fetchingData(){
+// const data = await getData();
+// console.log(data);
+//} Async await reemplaza el then y el catch
+
 module.exports = router;
